31d089407ee9fb5d12055f198028c48d86e883cf
Elissa <elissa.matsushita@gmail.com>
2020-01-07 22:59:54 +0000

    equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.toString() === arr2.toString()){
+    return true;
+  } return false; 
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

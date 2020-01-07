4bbf20b56bb396d6ae790115536155e03c7f768f
Elissa <elissa.matsushita@gmail.com>
2020-01-07 23:18:36 +0000

    assert Equal Arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.toString() === arr2.toString()){
+    return true;
+  } return false; 
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if(eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else 
+  
+  {console.log(`❌ ❌ ❌ Assertion Failed: ${arr1} !==  ${arr2}`)};
+}
+
+assertArraysEqual([1, 2, 3], [1, 3, 3]);

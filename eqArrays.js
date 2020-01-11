5444dcc9d780c8e273d44a97baa6bf573c23968b
Elissa <elissa.matsushita@gmail.com>
2020-01-11 18:21:04 +0000

    solves nested arrays using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -7,11 +7,36 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function (arr1, arr2) {
-  if (arr1.toString() === arr2.toString()){
-    return true;
-  } return false; 
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    }
+    if (Array.isArray(arr1[i]) && !(Array.isArray(arr2[i]))) {
+      return false;
+    } 
+    else if (!(Array.isArray(arr1[i])) && Array.isArray(arr2[i])) {
+      return false;
+    }
+  }
+
+  if (arr1.toString() !== arr2.toString()) {
+    return false;
+  }
+  return true;
+
 };
 
 
+assertEqual(  eqArrays([[2, 3], [4]], [[2, 3], [4]]), true   ) // => true
+assertEqual(  eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])  , false) // => false
+
+assertEqual(  eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

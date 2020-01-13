fdaca238416e35566a839630e6fab834180bb5f6
Elissa <elissa.matsushita@gmail.com>
2020-01-13 20:34:53 +0000

    added eqArrays test

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const eqArrays = function (arr1, arr2) {
 
   if (arr1.length !== arr2.length) {
@@ -33,10 +25,5 @@ const eqArrays = function (arr1, arr2) {
 
 };
 
+module.exports = eqArrays;
 
-assertEqual(  eqArrays([[2, 3], [4]], [[2, 3], [4]]), true   ) // => true
-assertEqual(  eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])  , false) // => false
-
-assertEqual(  eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
--- tail.js
+++ tail.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail
+
